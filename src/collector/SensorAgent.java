8cf6dfbff74614b7838d011f7dbaf59f3219abfb
David Levayer <david.levayer@gmail.com>
2015-04-12 20:15:57 -0400

    setup method for sensor agent

--- src/collector/SensorAgent.java
+++ src/collector/SensorAgent.java
@@ -1,15 +1,80 @@
 package collector;
 
+import java.util.Map;
+
+import collector.sensor.SensorInterface;
+import collector.sensor.TemperatureSensor;
+import collector.sensor.TimeSensor;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 
 public class SensorAgent extends Agent {
 
 	private static final long serialVersionUID = 1L;
 	
+	Map<Integer,SensorInterface<?>> mSensors;
+	
 	/**
-	 * Fonction appel??e lors de la cr??ation de l'agent
+	 * Fonction appelée lors de la création de l'agent
 	 */
 	protected void setup() {
+		
+		// Créer la liste des capteurs associés
+		initiateSensors();
+		
+		// Enregistrement de la liste des services auprès d'un annuaire
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("sensor-station");
+		sd.setName("JADE-sensor-station");
+		dfd.addServices(sd);
+		try {
+			DFService.register(this, dfd);
+		}
+		catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+
+		// Ajout d'un service/comportement : requête pour demander des informations
+		// sur les capteurs présents sur la station
+		addBehaviour(new OfferRequestsServer());
+
+		// Ajout d'un service/comportement : récupérer les valeurs des capteurs présents
+		addBehaviour(new SensorValueServer());
+		
+	}
+	
+	private void initiateSensors(){
+		mSensors.put(SensorInterface.TIME_SENSOR, new TimeSensor());
+		mSensors.put(SensorInterface.TEMPERATURE_SENSOR, new TemperatureSensor());
+	}
+	
+	private class OfferRequestsServer extends CyclicBehaviour {
+
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public void action() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	private class SensorValueServer extends CyclicBehaviour {
+
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public void action() {
+			// TODO Auto-generated method stub
+			
+		}
 		
 	}
 
