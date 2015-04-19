d5c898e7d6d125e0f8d9ed32a05d7767df5bc4af
David Levayer <david.levayer@gmail.com>
2015-04-19 10:34:12 -0400

    debug: correct float separator

--- src/collector/TemperatureSensor.java
+++ src/collector/TemperatureSensor.java
@@ -1,6 +1,7 @@
 package collector;
 
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -8,36 +9,39 @@ import java.util.TimerTask;
 public class TemperatureSensor implements Sensor<Float> {
 
 	private final static int valueSize = 10;
-	
+
 	private final static float minTemperature = 15f;
 	private final static float maxTemperature = 30f;
-	
+
 	private Random rand;
 	private Float[] values;
 	private int pos;
-	
+
 	public TemperatureSensor() {
-		
+
 		rand = new Random();
 		values = new Float[valueSize];
 		pos = 0; 
-		
+
 		Timer timer = new Timer();
 		TimerTask minuteTask = new TimerTask() {
-			
+
 			@Override
 			public void run() {
 				float value = minTemperature + (rand.nextFloat() * ((1 + maxTemperature) - minTemperature));
-				DecimalFormat df = new DecimalFormat(); 
+				DecimalFormat df = new DecimalFormat();
+				DecimalFormatSymbols dfs = new DecimalFormatSymbols();
+				dfs.setDecimalSeparator('.');
+				df.setDecimalFormatSymbols(dfs);
 				df.setMaximumFractionDigits(1);
 				values[pos] = Float.valueOf(df.format(value));
 				pos = (pos + 1) % valueSize;
 			}
 		};
-		
+
 		timer.scheduleAtFixedRate(minuteTask, 0, 2000);
 	}
-	
+
 	@Override
 	public int getType() {
 		return Sensor.TEMPERATURE_SENSOR;
